8f80d692871ca6e86693767e1653497333330e7d
VoidNull <ethan173@icloud.com>
2021-01-14 14:27:52 -0600

    Added the capacity to add or remove timestamps

--- __main__.py
+++ __main__.py
@@ -11,7 +11,7 @@ except ModuleNotFoundError:
 
 location = sys.argv[0]
 os.system("clear")
-print("si quieres añadir tu horario abre el .csv o corre con -v \npara parar usa ^c (control+c) en la terminal\nrecuerda que el programa solo iniciara cuando cierres completamente tu editor de tablas")
+print("If you want to add your schedule run with -v flag\nto stop use ^c (control+c) keyboardInterrupt\n")
 try:
     sys.argv.index("-v")
     v = True
@@ -20,62 +20,45 @@ except ValueError:
     pass
 if v:
     if sys.platform == "darwin":
+        print("Appliciation won't start untill the .csv editor is fully closed")
         os.system("open "+location.replace("__main__.py", "") + "schedule.csv -W")
     else:
         print("only works in OS X")
 print("Started")
 
 
-f = open(location.replace("__main__.py", "") + "schedule.csv", "r+")
-dayDictionary = {"Date": "Empty"}
-for line in f.readlines():
-    if line.lower().startswith("date"):
-        continue
-    line = line.strip('\n')
-    day = line.split(",")
-    dayDictionary[day.pop(0)] = day
-f.close()
-meetingSchedule = [dayDictionary["Monday"],
-                   dayDictionary["Tuesday"],
-                   dayDictionary["Wednesday"],
-                   dayDictionary["Thursday"],
-                   dayDictionary["Friday"],
-                   dayDictionary["Saturday"],
-                   dayDictionary["Sunday"]]
-day = 0
+with open(location.replace("__main__.py", "") + "schedule.csv", "r+") as f:
+    rows = [row.strip('\n').split(",") for row in f.readlines()]
+columns = []
+column_count = len(rows[0])
+column = []
+for i in range(column_count):
+    for row in rows:
+        column.append(row[i])
+    columns.append(column)
+    column = []
+hour_dict = {"*":""}
+for column in columns:
+    hour_dict[column.pop(0)] = column
 while True:
-    date = datetime.datetime.now()
-    old = date.hour
-    new = date.hour
-    while old == new:
-        try:
-            date = datetime.datetime.now()
-            day = date.today().weekday()
-            new = date.hour
-            sleep(1)
-        except KeyboardInterrupt:
-            sys.exit(0)
-    try:
-        print(meetingSchedule[day][date.hour])
-        print(day)
-        print(date.hour)
-        if meetingSchedule[day][date.hour] != "":
-            os.system("open -a /Applications/zoom.us.app &")
-            sleep(1)
-            keyboard.press_and_release('command+j')
-            sleep(1)
-            keyboard.press_and_release('command+a')
-            keyboard.press_and_release('delete')
-            sleep(1)
-            pyperclip.copy(meetingSchedule[day][date.hour])
-            keyboard.write(meetingSchedule[day][date.hour])
-            print(meetingSchedule[day][date.hour])
-            if meetingSchedule[day][date.hour] == "":
-                print("Nada")
-            keyboard.press_and_release('enter')
-            sleep(2400)
-        else:
-            print("Skipped")
-    except IndexError:
-        print("Skipped")
-        pass
+    while True:
+        sleep(60)
+        now = datetime.datetime.now()
+        hour = now.strftime('%H:%M')
+        print(hour)
+        hour_classes = hour_dict.get(hour, "")
+        if hour_classes != "":
+            meetCode = (hour_classes[now.weekday()])
+            break
+
+    print(meetCode)
+    os.system("open -a /Applications/zoom.us.app &")
+    sleep(1)
+    keyboard.press_and_release('command+j')
+    sleep(1)
+    keyboard.press_and_release('command+a')
+    keyboard.press_and_release('delete')
+    sleep(1)
+    pyperclip.copy(meetCode)
+    keyboard.write(meetCode)
+    keyboard.press_and_release('enter')
